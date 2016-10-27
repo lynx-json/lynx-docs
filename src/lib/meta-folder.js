4f6e7a7a1612fcea166c0218289a6523b830d108
Anson <anson.goldade@streamtext.net>
2016-10-27 08:42:31 -0500

    Created Realm constructor to represent realm meta data and functions to traverse

--- src/lib/meta-folder.js
+++ src/lib/meta-folder.js
@@ -7,69 +7,72 @@ const templatePattern = /^(.*)\.lynx\.yml$/;
 const dataFolderPattern = /^(.*)\.data$/;
 const dataFilePattern = /^(.*)\.data(\.(.*))?\.yml$/;
 
-module.exports = exports = folderPath => {
+function createVariant(name, templateFile, dataFile) {
+  return {
+    name: name,
+    template: templateFile,
+    data: dataFile
+  };
+}
 
-  function createVariant(name, templateFile, dataFile) {
-    return {
-      name: name,
-      template: templateFile,
-      data: dataFile
-    };
-  }
-
-  function deriveVariantsForTemplateFile(templateFile, templateName, contents) {
-      var templateVariants = [];
-      //add data files for the template
-      contents.forEach(function(item){
-        var result = dataFilePattern.exec(item);
-        if(!result || !result[1] === templateName) return;
-        var name = result[3] || templateName;
-        templateVariants.push(createVariant(name, templateFile.base, item));
-      });
-      //add files from data folders for the template
-      contents.forEach(function(item){
-        var result = dataFolderPattern.exec(item);
-        if(!result || result[1] !== templateName) return;
-
-        fs.readdirSync(path.resolve(templateFile.dir, item)).forEach(function(data){
-          var dataFilePath = path.join(item, data);
-          var dataName = path.parse(data).name;
-          var name = templateName === dataName ? dataName: templateName + "-" + dataName;
-          templateVariants.push(createVariant(name, templateFile.base, dataFilePath));
-        });
-      });
-      //if there are no data files, then the template itself is the variant
-      if(templateVariants.length === 0) templateVariants.push(createVariant(templateName, templateFile.base));
-
-      return templateVariants;
-  }
-
-  function deriveFromFileSystem(folder){
-    var contents = fs.readdirSync(folder);
-
-    var realm = { name: folder, realms: [], variants: [] };
-
-    realm.realms = contents.filter(function(item){
-      var stats = fs.statSync(path.resolve(folder, item));
-      return stats.isDirectory() && !dataFolderPattern.test(item);
+function deriveVariantsForTemplateFile(templateFile, templateName, contents) {
+    var templateVariants = [];
+    //add data files for the template
+    contents.forEach(function(item){
+      var result = dataFilePattern.exec(item);
+      if(!result || result[1] !== templateName) return;
+      var name = result[3] || templateName;
+      templateVariants.push(createVariant(name, templateFile.base, item));
     });
+    //add files from data folders for the template
+    contents.forEach(function(item){
+      var result = dataFolderPattern.exec(item);
+      if(!result || result[1] !== templateName) return;
 
+      fs.readdirSync(path.join(templateFile.dir, item)).forEach(function(data){
+        var dataFilePath = path.join(item, data);
+        var dataName = path.parse(data).name;
+        var name = templateName === dataName ? dataName: templateName + "-" + dataName;
+        templateVariants.push(createVariant(name, templateFile.base, dataFilePath));
+      });
+    });
+    //if there are no data files, then the template itself is the variant
+    if(templateVariants.length === 0) templateVariants.push(createVariant(templateName, templateFile.base));
+
+    return templateVariants;
+}
+
+function Realm(realmName) {
+  var self = this;
+  var parsedPath = path.parse(realmName);
+  var dir = parsedPath.dir ? "/" + parsedPath.dir + "/" : "/";
+  self.name = dir + parsedPath.base + "/";
+  var contents = fs.readdirSync(realmName);
+
+  self.getRealms = function getRealms(){
+    return contents.filter(function(item){
+      var stats = fs.statSync(path.join(realmName, item));
+      return stats.isDirectory() && !dataFolderPattern.test(item);
+    })
+    .map(function(item){
+      return new Realm(path.join(self.name.substr(1), item));
+    });
+  };
+
+  self.getVariants = function getVariants(){
+    var variants = [];
     contents.forEach(function(item) {
       var result = templatePattern.exec(item);
       if(!result) return;
-      var templateFile = path.parse(path.join(folder, item));
+      var templateFile = path.parse(path.join(realmName, item));
       var templateVariants = deriveVariantsForTemplateFile(templateFile, result[1], contents);
-      Array.prototype.push.apply(realm.variants, templateVariants);
+      Array.prototype.push.apply(variants, templateVariants);
     });
 
-    return realm;
-  }
+    return variants;
+  };
+}
 
-  function getRealm(folder) {
-
-    var realm = deriveFromFileSystem(folder);
-    return realm;
-  }
-
-  return getRealm(folderPath);
+module.exports = exports = folderPath => {
+  return new Realm(folderPath);
 };
--- test/lib/meta-folder.js
+++ test/lib/meta-folder.js
@@ -13,46 +13,62 @@ function statsStub(isDirectory) {
 }
 
 describe.only("when getting metadata for a folder", function () {
-  var meta;
 
   afterEach(function () {
     fs.readdirSync.restore();
-    fs.statSync.restore();
+    if(fs.statSync.restore) fs.statSync.restore();
+  });
+
+  describe("a folder with name 'x'", function (){
+    var realm;
+
+    beforeEach(function () {
+      sinon.stub(fs, "readdirSync").withArgs("x").returns([]);
+      realm =  getFolderMetadata("x");
+    });
+
+    it("should have a realm name of '/x/'", function () {
+      realm.name.should.equal("/x/");
+    });
   });
 
   describe("a folder with template ['one.lynx.yml']", function () {
+    var variants;
+
     beforeEach(function () {
-      sinon.stub(fs, "readdirSync").withArgs("/x").returns(["one.lynx.yml"]);
-      sinon.stub(fs, "statSync").withArgs("/x/one.lynx.yml").returns(statsStub(false));
-      meta = getFolderMetadata("/x");
+      sinon.stub(fs, "readdirSync").withArgs("x").returns(["one.lynx.yml"]);
+      sinon.stub(fs, "statSync").withArgs("x/one.lynx.yml").returns(statsStub(false));
+      variants =  getFolderMetadata("x").getVariants();
     });
 
-    it("should have a single state, 'one'", function () {
-      meta.variants.length.should.equal(1);
-      meta.variants[0].name.should.equal("one");
+    it("should have a variants ['one']", function () {
+      variants.length.should.equal(1);
+      variants[0].name.should.equal("one");
     });
 
     it("should have a value for template only", function () {
-      should.exist(meta.variants[0].template);
-      should.not.exist(meta.variants[0].data);
+      should.exist(variants[0].template);
+      should.not.exist(variants[0].data);
     });
   });
 
   describe("a folder with templates ['default.lynx.yml', 'two.lynx.yml']", function () {
+    var variants;
+
     beforeEach(function () {
-      sinon.stub(fs, "readdirSync").withArgs("/x").returns(["default.lynx.yml", "two.lynx.yml"]);
+      sinon.stub(fs, "readdirSync").withArgs("x").returns(["default.lynx.yml", "two.lynx.yml"]);
       sinon.stub(fs, "statSync").returns(statsStub(false));
-      meta = getFolderMetadata("/x");
+      variants = getFolderMetadata("x").getVariants();
     });
 
-    it("should have two states, 'default' and 'two'", function () {
-      meta.variants.length.should.equal(2);
-      meta.variants[0].name.should.equal("default");
-      meta.variants[1].name.should.equal("two");
+    it("should have variants ['default', 'two']", function () {
+      variants.length.should.equal(2);
+      variants[0].name.should.equal("default");
+      variants[1].name.should.equal("two");
     });
 
     it("should have a value for template only", function () {
-      meta.variants.forEach(function(variant){
+      variants.forEach(function(variant){
         should.exist(variant.template);
         should.not.exist(variant.data);
       })
@@ -60,85 +76,117 @@ describe.only("when getting metadata for a folder", function () {
 
   });
 
-  describe("a folder with template ['one.lynx.yml'] and data ['one.data.yml']", function () {
+  describe("a folder with templates ['default.lynx.yml', 'two.lynx.yml'] and data ['default.data.yml']", function () {
+    var variants;
+
     beforeEach(function () {
-      sinon.stub(fs, "readdirSync").withArgs("/x").returns(["one.lynx.yml", "one.data.yml"]);
+      sinon.stub(fs, "readdirSync").withArgs("x").returns(["default.lynx.yml", "two.lynx.yml", "default.data.yml"]);
       sinon.stub(fs, "statSync").returns(statsStub(false));
-      meta = getFolderMetadata("/x");
+      variants = getFolderMetadata("x").getVariants();
     });
 
-    it("should have one variant, 'one'", function () {
-      meta.variants.length.should.equal(1);
-      meta.variants[0].name.should.equal("one");
+    it("should have variants ['default', 'two']", function () {
+      variants.length.should.equal(2);
+      variants[0].name.should.equal("default");
+      variants[1].name.should.equal("two");
+    });
+
+    it("'default' should have a value for template and data", function () {
+      should.exist(variants[0].template);
+      should.exist(variants[0].data);
+    });
+
+    it("'two' should have a value for template only", function () {
+      should.exist(variants[1].template);
+      should.not.exist(variants[1].data);
+    });
+  });
+
+  describe("a folder with template ['one.lynx.yml'] and data ['one.data.yml']", function () {
+    var variants;
+
+    beforeEach(function () {
+      sinon.stub(fs, "readdirSync").withArgs("x").returns(["one.lynx.yml", "one.data.yml"]);
+      sinon.stub(fs, "statSync").returns(statsStub(false));
+      variants = getFolderMetadata("x").getVariants();
+    });
+
+    it("should have variants ['one']", function () {
+      variants.length.should.equal(1);
+      variants[0].name.should.equal("one");
     });
 
     it("should have a value for template and data", function () {
-      should.exist(meta.variants[0].template);
-      should.exist(meta.variants[0].data);
+      should.exist(variants[0].template);
+      should.exist(variants[0].data);
     });
   });
 
   describe("a folder with template ['one.lynx.yml'] and data ['one.data.variant.yml']", function () {
+    var variants;
+
     beforeEach(function () {
-      sinon.stub(fs, "readdirSync").withArgs("/x").returns(["one.lynx.yml", "one.data.variant.yml"]);
+      sinon.stub(fs, "readdirSync").withArgs("x").returns(["one.lynx.yml", "one.data.variant.yml"]);
       sinon.stub(fs, "statSync").returns(statsStub(false));
-      meta = getFolderMetadata("/x");
+      variants = getFolderMetadata("x").getVariants();
     });
 
-    it("should have one variant, 'variant'", function () {
-      meta.variants.length.should.equal(1);
-      meta.variants[0].name.should.equal("variant");
+    it("should have variants ['variant']", function () {
+      variants.length.should.equal(1);
+      variants[0].name.should.equal("variant");
     });
 
     it("should have a value for template and data", function () {
-      should.exist(meta.variants[0].template);
-      should.exist(meta.variants[0].data);
+      should.exist(variants[0].template);
+      should.exist(variants[0].data);
     });
   });
 
   describe("a folder with template ['default.lynx.yml'] and data ['default.data'] that contains ['default.yml']", function () {
+    var variants;
 
     beforeEach(function () {
       var readDirStuf = sinon.stub(fs, "readdirSync");
-      readDirStuf.withArgs("/x").returns(["default.lynx.yml", "default.data"]);
-      readDirStuf.withArgs("/x/default.data").returns(["default.yml"]);
+      readDirStuf.withArgs("x").returns(["default.lynx.yml", "default.data"]);
+      readDirStuf.withArgs("x/default.data").returns(["default.yml"]);
       var statStub = sinon.stub(fs, "statSync");
-      statStub.withArgs("/x/default.data").returns(statsStub(true));
+      statStub.withArgs("x/default.data").returns(statsStub(true));
       statStub.returns(statsStub(false));
-      meta = getFolderMetadata("/x");
+      variants = getFolderMetadata("x").getVariants();
     });
 
-    it("should have one state, 'default'", function () {
-      meta.variants.length.should.equal(1);
-      meta.variants[0].name.should.equal("default");
+    it("should have variants ['default']", function () {
+      variants.length.should.equal(1);
+      variants[0].name.should.equal("default");
     });
 
     it("should have a value for template and data", function () {
-      should.exist(meta.variants[0].template);
-      should.exist(meta.variants[0].data);
+      should.exist(variants[0].template);
+      should.exist(variants[0].data);
     });
   });
 
   describe("a folder with template ['one.lynx.yml'] and data ['one.data'] that contains ['default.yml', 'invalid.yml']", function () {
+    var variants;
 
     beforeEach(function () {
       var readDirStuf = sinon.stub(fs, "readdirSync");
-      readDirStuf.withArgs("/x").returns(["one.lynx.yml", "one.data"]);
-      readDirStuf.withArgs("/x/one.data").returns(["default.yml", "invalid.yml"]);
+      readDirStuf.withArgs("x").returns(["one.lynx.yml", "one.data"]);
+      readDirStuf.withArgs("x/one.data").returns(["default.yml", "invalid.yml"]);
       var statStub = sinon.stub(fs, "statSync");
-      statStub.withArgs("/x/one.data").returns(statsStub(true));
+      statStub.withArgs("x/one.data").returns(statsStub(true));
       statStub.returns(statsStub(false));
-      meta = getFolderMetadata("/x");
+      variants = getFolderMetadata("x").getVariants();
     });
 
-    it("should have two states, 'one-default' and 'one-invalid'", function () {
-      meta.variants.length.should.equal(2);
-      meta.variants[0].name.should.equal("one-default");
-      meta.variants[1].name.should.equal("one-invalid");
+    it("should have variants ['one-default', 'one-invalid']", function () {
+      variants.length.should.equal(2);
+      variants[0].name.should.equal("one-default");
+      variants[1].name.should.equal("one-invalid");
     });
 
     it("should have a value for template and data", function () {
-      meta.variants.forEach(function(variant){
+      variants.forEach(function(variant){
         should.exist(variant.template);
         should.exist(variant.data);
       })
