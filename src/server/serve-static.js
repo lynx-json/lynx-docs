22249c0c5fd339ffa86d78a9fd9f13312f3ff605
Dan Mork <danmork@gmail.com>
2016-11-01 17:45:03 -0500

    ported serve-static code

--- src/server/serve-static.js
+++ src/server/serve-static.js
@@ -1,6 +1,50 @@
-module.exports = exports = function createStaticHandler(options) {
-  // try to find the static file or call next
-  return function (req, res, next) {
-    next();
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+const mime = require("mime");
+
+function serveFile(req, res, next) {
+  function onFileContents(err, contents) {
+    if (err) {
+      console.log(err);
+      return next();
+    }
+    
+    res.writeHead(200, { "Content-Type": mime.lookup(req.filename) });
+    res.write(contents, "binary");
+    res.end();
+  }
+  
+  function onFileStat(err, stat) {
+    if (err) {
+      console.log(err);
+      return next();
+    }
+    
+    if (stat.isDirectory()) return next();
+    fs.readFile(req.filename, "binary", onFileContents);
+  }
+  
+  fs.stat(req.filename, onFileStat);
+}
+
+function createServeStatic(options) {
+  return function serveStatic(req, res, next) {
+    var relativePathToFile = "." + url.parse(req.url).pathname;
+    if (!path.extname(relativePathToFile)) return next();
+    
+    var pathsToFile = options.root.map(function (root) {
+      return path.resolve(root, relativePathToFile);
+    });
+    
+    var pathToFile = pathsToFile.find(function (p) {
+      return fs.existsSync(p);
+    });
+    
+    if (!pathToFile) return next();
+    req.filename = pathToFile;
+    serveFile(req, res, next);
   };
-};
+}
+
+module.exports = exports = createServeStatic;
