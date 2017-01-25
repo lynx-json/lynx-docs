298e305594a78febb5819493453ccaa395814bba
Anson Goldade <anson.goldade@streamtext.net>
2017-01-25 08:58:35 -0600

    Checkpoint on partial tests

--- test/lib/partials/group.js
+++ test/lib/partials/group.js
@@ -5,48 +5,100 @@ const should = chai.should();
 chai.use(chaiAsPromised);
 const partials = require("../../../src/lib/partials-yaml");
 
-describe("group partial", function () {
-  it("should describe an array value with the 'group' hint", function () {
-    var sourceKVP = {
+var tests = [{
+    description: "array value",
+    should: "have group hint and array value",
+    kvp: {
       key: ">group",
-      value: [
-        "one",
-        "two",
-        "three"
-      ]
-    };
-
-    var result = partials.getPartial(sourceKVP, {
-      realm: {
-        folder: process.cwd()
+      value: ["one", "two", "three"]
+    },
+    expected: {
+      key: "",
+      value: {
+        spec: { hints: ["group"] },
+        value: ["one", "two", "three"]
       }
-    });
-
-    result.value.spec.hints[0].should.equal("group");
-    result.value.value[0].should.equal("one");
-    result.value.value[1].should.equal("two");
-    result.value.value[2].should.equal("three");
-  });
-
-  it("should describe an object value with the 'group' hint", function () {
-    var sourceKVP = {
+    }
+  },
+  {
+    description: "object value",
+    should: "have group hint and object value",
+    kvp: {
+      key: ">group",
+      value: { one: "one", two: "two", three: "three" }
+    },
+    expected: {
+      key: "",
+      value: {
+        spec: { hints: ["group"] },
+        value: { one: "one", two: "two", three: "three" }
+      }
+    }
+  },
+  {
+    description: "with spec.* properties",
+    should: "have specified spec properties in result",
+    kvp: {
       key: ">group",
       value: {
-        one: "one",
-        two: "two",
-        three: "three"
+        "spec.hints": ["whatever", "group"],
+        "spec.visibility": "visible",
+        "spec.input": true,
+        value: ["one", "two"]
       }
-    };
+    },
+    expected: {
+      key: "",
+      value: {
+        spec: {
+          hints: ["whatever", "group"],
+          visibility: "visible",
+          "input": true
+        },
+        value: ["one", "two"]
+      }
+    }
+  },
+  {
+    description: "with spec object",
+    should: "have specified spec properties in result",
+    kvp: {
+      key: ">group",
+      value: {
+        spec: {
+          hints: ["group"],
+          visibility: "visible",
+          "input": true
+        },
+        value: ["one", "two"]
+      }
+    },
+    expected: {
+      key: "",
+      value: {
+        spec: {
+          hints: ["group"],
+          visibility: "visible",
+          "input": true
+        },
+        value: ["one", "two"]
+      }
+    }
+  }
+];
 
-    var result = partials.getPartial(sourceKVP, {
-      realm: {
-        folder: process.cwd()
-      }
+describe.only("group partials", function () {
+  tests.forEach(test => {
+    describe(test.description, function () {
+      it("should ".concat(test.should), function () {
+        runTest(test);
+      });
     });
-
-    result.value.spec.hints[0].should.equal("group");
-    result.value.value.one.should.equal(sourceKVP.value.one);
-    result.value.value.two.should.equal(sourceKVP.value.two);
-    result.value.value.three.should.equal(sourceKVP.value.three);
   });
 });
+
+function runTest(test) {
+  var actual = partials.getPartial(test.kvp, { realm: { folder: process.cwd() } });
+  delete actual.location; //TODO: Understand why this is returned
+  actual.should.deep.equal(test.expected);
+}
