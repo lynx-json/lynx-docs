5ba3d9a58d72234e12dfea3cf682b8609a2a323c
Anson Goldade <anson.goldade@streamtext.net>
2017-02-17 19:13:24 -0600

    More fixes for addSpecChildren. Docs render now

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 
 const util = require("util");
 const url = require("url");
+const expandYaml = require("./lib/expand-yaml");
 
 module.exports = exports = function (lynxDocs) {
   var finishYaml = lynxDocs.lib.finish;
@@ -57,28 +58,49 @@ module.exports = exports = function (lynxDocs) {
     var node = kvp.value;
     node.spec.children = node.spec.children || [];
 
-    function addChildNode(childMeta) {
+    function addChildNode(childMeta, childKey) {
       function match(childSpec) {
-        return childSpec.name === childMeta.key;
+        return childSpec.name === childKey;
       }
 
       if(childMeta.more) childMeta = childMeta.more();
 
-      if(!isNode(childMeta)) return;
+      if(!isNode(childMeta) && !childMeta.templates) return;
       if(node.spec.children.some(match)) return;
 
-      node.spec.children.push({ name: childMeta.key });
+      node.spec.children.push({ name: childKey });
+    }
+
+    function findTemplateWithChildren(templates) {
+      var templatesWithChildren = templates.map(t => {
+          if(t.more) return t.more();
+          return t;
+        })
+        .filter(t => t.children);
+      if(templatesWithChildren.length > 1) {
+        var compare = Object.getOwnPropertyNames(templatesWithChildren[0].children).sort().join();
+        for(var i = 1; i < templatesWithChildren.length; i++) {
+          if(Object.getOwnPropertyNames(templatesWithChildren[i].children).sort().join() !== compare) {
+            throw new Error("Two value templates have different children. This is an authoring error.");
+          }
+        }
+      }
+      if(templatesWithChildren.length === 0) return templates[0];
+      return templatesWithChildren[0];
     }
 
     var valueMeta = meta.children.value;
-
-    if(valueMeta.templates) valueMeta = valueMeta.templates[0];
     if(valueMeta.more) valueMeta = valueMeta.more();
 
+    if(valueMeta.templates) valueMeta = findTemplateWithChildren(valueMeta.templates);
+
     if(valueMeta.children) {
-      Object.getOwnPropertyNames(valueMeta.children).forEach(childKey => {
-        addChildNode(valueMeta.children[childKey]);
-      });
+      Object.getOwnPropertyNames(valueMeta.children)
+        .filter(childKey => !expandYaml.excludes.some(f => f({ key: childKey })))
+        .forEach(childKey => {
+          var childMeta = valueMeta.children[childKey];
+          addChildNode(childMeta, childKey);
+        });
     }
   });
 };
