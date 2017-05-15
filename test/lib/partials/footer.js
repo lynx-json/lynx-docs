38bd0d87a92ad040549515fd35a6967ff66deb25
John Howes <john.howes@gmail.com>
2017-05-15 18:55:13 -0500

    Footer

---
+++ test/lib/partials/footer.js
@@ -0,0 +1,93 @@
+var tests = [{
+    description: "when array value",
+    should: "have footer hint and array value",
+    parameters: ["one", "two", "three"],
+    expected: {
+      spec: { hints: ["footer", "container"] },
+      value: ["one", "two", "three"]
+    }
+  },
+  {
+    description: "when object value",
+    should: "have footer hint and object value",
+    parameters: { one: "one", two: "two", three: "three" },
+    expected: {
+      spec: { hints: ["footer", "container"] },
+      value: { one: "one", two: "two", three: "three" }
+    }
+  },
+  {
+    description: "when spec.* properties",
+    should: "have specified spec.* properties in result",
+    parameters: {
+      "spec.hints": ["whatever", "footer"],
+      "spec.visibility": "visible",
+      "spec.input": true,
+      value: ["one", "two"]
+    },
+    expected: {
+      spec: {
+        hints: ["whatever", "footer"],
+        visibility: "visible",
+        input: true
+      },
+      value: ["one", "two"]
+    }
+  },
+  {
+    description: "when spec.hints",
+    should: "override default hints",
+    parameters: {
+      "spec.hints": ["whatever"],
+      value: ["one", "two"]
+    },
+    expected: {
+      spec: {
+        hints: ["whatever"],
+      },
+      value: ["one", "two"]
+    }
+  },
+  {
+    description: "when fully specified spec object",
+    should: "use provided spec object. Don't default hints",
+    parameters: {
+      spec: {
+        hints: ["whatever"],
+        visibility: "visible",
+        input: true
+      },
+      value: ["one", "two"]
+    },
+    expected: {
+      spec: {
+        hints: ["whatever"],
+        visibility: "visible",
+        input: true
+      },
+      value: ["one", "two"]
+    }
+  },
+  {
+    description: "when flattened value",
+    should: "copy to 'value' key",
+    parameters: ["one", "two"],
+    expected: {
+      spec: { hints: ["footer", "container"] },
+      value: ["one", "two"]
+    }
+  },
+  {
+    description: "when expanded value",
+    should: "copy input 'value' to 'value' key",
+    parameters: { value: ["one", "two", "three"] },
+    expected: {
+      spec: { hints: ["footer", "container"] },
+      value: ["one", "two", "three"]
+    }
+  },
+];
+
+tests.partial = "footer";
+
+module.exports = tests;
