6b87d10ee9b0eed5fadea0026c227707a781bf18
Anson Goldade <anson@goldade.us>
2018-01-09 10:29:14 -0600

    Resolve Issue #72 (#74)

--- src/lib/export/lynx/validateDocument/form.js
+++ src/lib/export/lynx/validateDocument/form.js
@@ -1,8 +1,8 @@
 const types = require("../../../../types");
 
 function validateForm(value) {
-  let valid = (types.isNull(value) || types.isObject(value));
-  return valid ? [] : ["'form' value must be an object"];
+  let valid = (types.isNull(value) || types.isObject(value) || types.isArray(value));
+  return valid ? [] : ["'form' value must be an object or array"];
 }
 
 module.exports = exports = validateForm;
