031764bbbd3d4f5f0e29b81896ebb3ee8f1e9d9a
Anson <anson.goldade@streamtext.net>
2018-05-15 15:14:05 -0500

    Explicit folder path calculation for export
    Use a function to calculate the folder that a file will be placed into rather than delegating that responsibility. This, in combination with calcualing spec paths as absolute values, resolves the spec calculation relative to process.cwd().

--- src/cli/stream-utils.js
+++ src/cli/stream-utils.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const path = require("path");
 const vfs = require("vinyl-fs");
 const vss = require("vinyl-source-stream");
 const vbuffer = require("vinyl-buffer");
@@ -7,19 +8,24 @@ const through2 = require("through2");
 const isStream = require("isstream");
 
 function createSourceStream(input) {
-  if(isStream(input)) return input.pipe(vss(input.path || "unknown")).pipe(vbuffer()); //convert stream to vinyl stream
+  if (isStream(input)) return input.pipe(vss(input.path || "unknown")).pipe(vbuffer()); //convert stream to vinyl stream
   return vfs.src(input);
 }
 
 function createDestStream(output) {
-  if(isStream(output)) return vinylToWritable(output); //convert from vinyl stream to regular stream
-  return vfs.dest(output);
+  if (isStream(output)) return vinylToWritable(output); //convert from vinyl stream to regular stream
+  const resolveFolder = function (vinyl) {
+    let absolute = path.isAbsolute(vinyl.path) ? vinyl.path : path.resolve(process.cwd(), output, vinyl.path);
+    return path.dirname(absolute);
+  };
+
+  return vfs.dest(resolveFolder);
 }
 
 function vinylToWritable(writeable) {
   return through2.obj(function (file, enc, cb) {
     writeable.write(file.contents.toString(), enc, cb);
-    if(writeable !== process.stdout) writeable.end();
+    if (writeable !== process.stdout) writeable.end();
   });
 }
 
